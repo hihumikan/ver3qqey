37ea57065b677037771e29d5d04d185de8bfa0cc
hihumikan <musecharo@gmail.com>
2021-04-26 01:50:02 +0900

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    "@nuxtjs/vuetify"
+    "@nuxtjs/vuetify",
+    "@nuxtjs/pwa"
   ],
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
