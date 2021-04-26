1ae2c1dfd1447530898c8aad2b8ebc18f57bf016
hihumikan <musecharo@gmail.com>
2021-04-26 16:25:14 +0900

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,11 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "" },
+      { name: "twitter:card", content: "summary" },
+      { name: "twitter:site", content: "@mikan_54951" },
+      { name: "twitter:card", content: "summary" },
+      { name: "og:image", content: "~/assets/icon.png" }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
